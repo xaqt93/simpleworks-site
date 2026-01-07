6b7d4d75a2faeb3c3b43772e714502fb2f2fd851
xaqt93 <75286839+xaqt93@users.noreply.github.com>
2026-01-06 20:58:07 -0500

    Update Page

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
             lineHeight: 1.5,
           }}
         >
-          Because dating should feel human again
+          Because dating should feel human again.
         </p>
 
         <div
